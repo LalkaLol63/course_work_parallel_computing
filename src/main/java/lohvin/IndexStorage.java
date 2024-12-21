fd4914a461c76c7c95ae9b86964be051f33d76b3
Maksym Lohvin <mm.logvin@gmail.com>
2024-12-21 21:12:12 +0200

    feat(index-storage): turn hash table into index storage. add basic functionality: put, get, addToSet

---
+++ src/main/java/lohvin/IndexStorage.java
@@ -0,0 +1,129 @@
+package lohvin;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class IndexStorage{
+    LinkedList<Entry>[] buckets;
+    private int numEntries = 0;
+    private int capacity = 10;
+    private static final double loadFactorLimit = 0.75;
+
+    public IndexStorage() {
+        buckets = (LinkedList<Entry>[]) new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public void put(String key, Set<String> value) {
+        if(checkLoadFactor()) {
+            updateMemory();
+        }
+        Entry entry = getEntry(key);
+        if(entry != null) {
+            entry.setValue(value);
+        } else {
+            int index = getIndex(key);
+            buckets[index].add(new Entry(key, value));
+            numEntries++;
+        }
+    }
+
+    public void addToSet(String key, String element) {
+        Entry entry = getEntry(key);
+        if (entry != null) {
+            entry.getValue().add(element);
+        } else {
+            Set<String> newSet = new HashSet<>();
+            newSet.add(element);
+            put(key, newSet);
+        }
+    }
+
+    public Set<String> searchByQuery(String query) {
+        String[] words = query.split(" ");
+        Set<String> files = get(words[0]);
+        if(files == null) {
+            return null;
+        }
+        Set<String> result = new HashSet<>(files);
+        for (int i = 1; i < words.length; i++) {
+            files = get(words[i]);
+            if(files == null) {
+                return null;
+            }
+            result.retainAll(files);
+        }
+        return result;
+    }
+
+    public Set<String> get(String key) {
+        Entry entry = getEntry(key);
+        if(entry != null) {
+            return entry.getValue();
+        }
+        return null;
+    }
+
+    private Entry getEntry(String key) {
+        int index = getIndex(key);
+        for (Entry entry : buckets[index]) {
+            if(entry.getKey().equals(key)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+    private void updateMemory() {
+        capacity *= 2;
+        LinkedList<Entry>[] newBuckets = (LinkedList<Entry>[]) new LinkedList[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            newBuckets[i] = new LinkedList<>();
+        }
+
+        for (LinkedList<Entry> bucket : buckets) {
+            for (Entry entry : bucket) {
+                int newIndex = getIndex(entry.getKey());
+                newBuckets[newIndex].add(entry);
+            }
+        }
+        buckets = newBuckets;
+    }
+
+    private int getIndex(String key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    private boolean checkLoadFactor() {
+        return (double)numEntries / capacity >= loadFactorLimit;
+    }
+
+    static class Entry {
+        private String key;
+        private Set<String> value;
+
+        public Entry(String key, Set<String> value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public Set<String> getValue() {
+            return value;
+        }
+
+        public void setValue(Set<String> value) {
+            this.value = value;
+        }
+    }
+}
