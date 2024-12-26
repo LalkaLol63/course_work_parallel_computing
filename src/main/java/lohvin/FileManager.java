bcdcdf0a1b00af29dd770c64e6f4803cdb347f01
Maksym Lohvin <mm.logvin@gmail.com>
2024-12-27 01:27:05 +0200

    feat(file-manager): implement Singleton pattern

--- src/main/java/lohvin/FileManager.java
+++ src/main/java/lohvin/FileManager.java
@@ -8,14 +8,30 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class FileManager {
+    private static volatile FileManager instance;
     private final Path baseDirectory;
 
-    public FileManager(Path baseDirectory) {
+    private FileManager(Path baseDirectory) {
         if (!Files.exists(baseDirectory) || !Files.isDirectory(baseDirectory)) {
             throw new IllegalArgumentException("Вказана базова директорія не дійсна: " + baseDirectory);
         }
         this.baseDirectory = baseDirectory;
     }
+
+    public synchronized static FileManager getInstance(Path baseDirectory) {
+        if (instance == null) {
+            instance = new FileManager(baseDirectory);
+        }
+        return instance;
+    }
+
+    public static FileManager getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("FileManager ще не ініціалізований." );
+        }
+        return instance;
+    }
+
 
     public String loadFile(String relativePath) {
         Path path = baseDirectory.resolve(relativePath);
